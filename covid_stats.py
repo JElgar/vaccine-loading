84ef7823f3b6a4473c18918e15fd302a01ee1e56
jelgar <jamesnelgar@gmail.com>
2021-02-03 23:54:03 +0000

    Get latest day

--- covid_stats.py
+++ covid_stats.py
@@ -1,6 +1,5 @@
 from requests import get
 from json import dumps
-from tqdm import tqdm
 
 UK_POP = 68_098_857
 
@@ -9,8 +8,8 @@ AREA_TYPE = "overview"
 AREA_NAME = "United Kingdom"
 DATE = "2021-02-01"
 
-# filters = [f"areaType={ AREA_TYPE }", f"areaName={ AREA_NAME }"]
-filters = [f"areaType={AREA_TYPE}", f"areaName={AREA_NAME}", f"date={DATE}"]
+filters = [f"areaType={AREA_TYPE}", f"areaName={AREA_NAME}"]
+# filters = [f"areaType={AREA_TYPE}", f"areaName={AREA_NAME}", f"date={DATE}"]
 
 structure = {
     "date": "date",
@@ -41,12 +40,10 @@ response = get(ENDPOINT, params=api_params, timeout=10)
 if response.status_code >= 400:
     raise RuntimeError(f"Request failed: { response.text }")
 
-for day in response.json()["data"]:
-    total_vaccines_first = day["vaccines"]["first_dose"]["cumulative"]
-    total_vaccines_second = day["vaccines"]["second_dose"]["cumulative"]
-    first_percentage = (total_vaccines_first / UK_POP) * 100
-    total_percentage = (
-        (total_vaccines_first + total_vaccines_second) / (UK_POP * 2)
-    ) * 100
-    print(first_percentage)
-    print(total_percentage)
+day = response.json()["data"][0]
+total_vaccines_first = day["vaccines"]["first_dose"]["cumulative"]
+total_vaccines_second = day["vaccines"]["second_dose"]["cumulative"]
+first_percentage = (total_vaccines_first / UK_POP) * 100
+total_percentage = ((total_vaccines_first + total_vaccines_second) / (UK_POP * 2)) * 100
+print(first_percentage)
+print(total_percentage)
